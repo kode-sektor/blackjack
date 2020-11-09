039c57b84364915bd12358fdc2f3b31b0c223af4
Olagoke Kay <60120856+Kardinalkay@users.noreply.github.com>
2020-11-09 11:01:44 -0500

    Add 'hit' functionality

--- src/components/App.js
+++ src/components/App.js
@@ -7,45 +7,62 @@ export default class App extends Component {
     state = {
 
         blackjackGame : {
-			'you' : {
-				'scoreSpan' : '#your-blackjack-result', 
-				'div' : '#your-box',
-				'score' : 0
+			you : {
+                score : 0, 
+                cardImage : []
 			},
-			'dealer' : {
-				'scoreSpan' : '#dealer-blackjack-result', 
-				'div' : '#dealer-box',
-				'score' : 0
+			dealer : {
+                score : 0, 
+                cardImage : ''
 			},
-			'cards' : ['2','3','4','5','6','7','8','9','10','K','J','Q','A'],
-			'cardsMap' : {'2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '10':10, 'K':10, 'J':10, 'Q':10, 'A':[1,11] },
-			'wins' : 0,
-			'losses' : 0,
-			'draws' : 0,
+			cards : ['2','3','4','5','6','7','8','9','10','K','J','Q','A'],
+			cardsMap : {'2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9, '10':10, 'K':10, 'J':10, 'Q':10, 'A':[1,11] },
+			wins : 0,
+			losses : 0,
+			draws : 0,
 
-			'isStand' : false,
-			'turnsOver' : false
+			isStand : false,
+			turnsOver : false
 		}
     }
 
     // On click of 'Hit', generate random card, update and show score
     blackjackHit = () => {
-
-        if (this.state.blackjackGame['isStand'] === false) {	 // 'Hit' should only work if stand button has not yet been clicked (COM has played)
-
-            let card = this.randomCard();	// get random card
-            alert(card)
-        }
-
+        console.log(this.state.blackjackGame['isStand'])
+        // if (this.state.blackjackGame['isStand'] === false) {	 // 'Hit' should only work if stand button has not yet been clicked (COM has played)
+            let card = this.randomCard()	// get random card
+            this.showCard(this.state.blackjackGame['you'], card) // .then(hitSound.play());	// show random card in HTML
+        // }
     }
 
     randomCard = () => {
         let randomIndx = Math.floor(Math.random() * 13);	// 13 possibilities
+        console.log(this.state.blackjackGame)
         return this.state.blackjackGame.cards[randomIndx];
+    }
+
+    // Create random image element and insert in HTML
+    showCard = (activePlayer, card) => {
+        // Set threshold to prevent game from continuing past score
+        alert('ok')
+        // if (activePlayer.score <= 21) {	 
+
+            this.setState({
+                blackjackGame : {
+                    ...this.state.blackjackGame,
+                    you : {
+                        ...this.state.blackjackGame.you,
+                        cardImage : [...this.state.blackjackGame.you.cardImage, <img src={`images/cards/${card}.png`} alt={`${card}`}/>]
+                    }
+                }
+            })
+        // }
     }
 
 
     render() {
+
+        console.log(this.state.blackjackGame.you.cardImage)
 
         return (
 
@@ -59,22 +76,29 @@ export default class App extends Component {
                     </h2>
                 </header>
                 
-                <div className="flex-blackjack-row-1">
+                <section className="flex-blackjack-row-1">
                     <div id="your-box">
                         <h2>You: <span id="your-blackjack-result">0</span></h2>
+                        <section>
+                            {(this.state.blackjackGame.you.cardImage).map((index, element) => {
+                                return (
+                                    index
+                                )
+                            })}
+                        </section>
                     </div>
                     <div id="dealer-box">
                         <h2>Dealer: <span id="dealer-blackjack-result">0</span></h2>
                     </div>
-                </div>
-                <div className="flex-blackjack-row-2">
+                </section>
+                <section className="flex-blackjack-row-2">
                     <ButtonGroup>
                         <Button onClick={(e)=> {this.blackjackHit()}} className="btn-lg btn-primary mr-2" id="blackjack-hit-button">Hit</Button>
                         <Button onClick={(e)=> {this.blackjackHit()}} className="btn-lg btn-danger mr-2" id="blackjack-deal-button">Deal</Button>
                         <Button onClick={(e)=> {this.blackjackHit()}} className="btn-lg btn-warning" id="blackjack-stand-button">Stand</Button>
                     </ButtonGroup>
-                </div>
-                <div className="flex-blackjack-row-3 result">
+                </section>
+                <section className="flex-blackjack-row-3 result">
                     <table>
                         <tbody>
                             <tr>
@@ -89,7 +113,7 @@ export default class App extends Component {
                             </tr>
                         </tbody>
                     </table>
-                </div>
+                </section>
         
             </div>
             
